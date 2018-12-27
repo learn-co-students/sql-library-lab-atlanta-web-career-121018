6b9f92374527ed16ca4acdb77c20a68573242183
github username <github email address>
2018-12-27 00:27:03 -0500

    Done.

---
+++ lib/update.sql
@@ -0,0 +1,4 @@
+
+
+-- INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (8, "Character Four", "motto four", "cyclon", 2, 2);
+UPDATE characters SET species = "Martian" WHERE id = 8;
